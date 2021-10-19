49bad5d5aca3649fb4a733d0981ed84be9913ac9
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-10-19 22:06:31 +0200

    update

--- chlg_10.js
+++ chlg_10.js
@@ -1,6 +1,5 @@
 function getCount(str) {
     var vowelsCount;
-    
     // enter your majic here
     for(let i=0; i<=str.length; i++){
        vowelsCount = str.replace(/[bcdfghjklmnpqrstvwxz| BCDFGHJKLMNPQRSTVWXZ]/g,"")
