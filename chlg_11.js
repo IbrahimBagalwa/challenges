913e2cfc52b46f5fe9f9134c0d99d9f8bbc99deb
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-12-01 15:43:03 +0200

    concat method

--- chlg_11.js
+++ chlg_11.js
@@ -1,4 +1,13 @@
+// charAt
 function accederChart(str){
     return str.charAt(3)
 }
 console.log(accederChart("Bonjour"))
+const phrase ="la methode renvoie la nouvelle chaine contenant le caractere a la position indiquee dans l'argument"
+const index = 7;
+console.log(`le caractere a l'index ${index} est ${phrase.charAt(index)}`);
+// la methode concat
+
+const h = "Hello";
+const w = "world";
+console.log(h.concat(' ' + w))
