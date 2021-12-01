becfe8ac80c7ca0f1e667702606ded317b238c8d
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-12-01 16:00:30 +0200

    update

--- chlg_11.js
+++ chlg_11.js
@@ -2,20 +2,20 @@
 function accederChart(str){
     return str.charAt(3)
 }
-console.log(accederChart("Bonjour"))
+console.log(accederChart("Bonjour"))//j
 const phrase ="la methode renvoie la nouvelle chaine contenant le caractere a la position indiquee dans l'argument"
 const index = 7;
-console.log(`le caractere a l'index ${index} est ${phrase.charAt(index)}`);
+console.log(`le caractere a l'index ${index} est ${phrase.charAt(index)}`); // o
 // la methode concat
 
 const h = "Hello";
 const w = "world";
-console.log(h.concat(' ' + w))
+console.log(h.concat(' ' + w))//hello word
 
 var b = ["Bonjour",' ', "Josh",' ', "!"]
-console.log("".concat(...b))
+console.log("".concat(...b))//bonjour josh !
 
 // includes() methode il renvoi true or false selon le cas de figure
 
 const word = "nouvelle";
-console.log(phrase.includes(word))
+console.log(phrase.includes(word))//true
