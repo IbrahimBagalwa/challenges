4394d63a5e66b5ffb7573f759de254bf477461c6
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-12-01 14:55:18 +0200

    update

---
+++ chlg_11.js
@@ -0,0 +1,4 @@
+function accederChart(str){
+    return str.charAt(3)
+}
+console.log(accederChart("Bonjour"))
--- jsLean.js
+++ jsLean.js
@@ -26,4 +26,41 @@ const [x,y] = myArray;
 const p = myArray[0];
 const m = myArray[1];
 
-console.log(p,m)//a b
+// console.log(p,m)//a b
+
+const students = [
+  { name: "Nick", grade: 10 },
+  { name: "John", grade: 15 },
+  { name: "Julia", grade: 19 },
+  { name: "Nathalie", grade: 9 },
+]
+const mapPlus = students.map(top => top.grade);
+
+const filterPlus = mapPlus.filter(ite=> ite >10);
+
+const reducePlus = filterPlus.reduce((prev, next)=> prev + next,0);
+
+// console.log(filterPlus,reducePlus)
+const numbers = [1,2,3,4,5,6,7,8,9];
+
+
+const pair = numbers.filter(function(n){
+    return n % 2 === 0
+});
+// console.log(pair)
+
+const double =(n)=> n*2;
+
+const doublesArry = numbers.map(double);
+// console.log(doublesArry)
+
+function grade(fsname,lsname, ...grades){
+    const grd = grades.reduce((prev, next)=>prev + next, 0)/grades.length
+    return{
+        name: fsname,
+        lsname: lsname,
+        grades: grades,
+        grd:grd
+    }
+}
+console.log(grade("Nick", "John",10, 12, 15, 60))
