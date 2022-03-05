8c28f0f00f5b21dd060a0433bb57cb9d4c003e37
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-03-05 16:33:04 +0200

    render Game

--- per/buldingGame/index.js
+++ per/buldingGame/index.js
@@ -1,18 +1,37 @@
-let firstCard =10;
-let secondCard = 11;
+let firstCard =5;
+let secondCard = 2;
 let message = ""
 let isAlive = true;
 let hasBlackJack = false;
 let sum = firstCard + secondCard;
 
 
-if(sum <= 20){
-    message = "Do you want to draw a new card? ";
-}else if(sum === 21){
-   message ="Wohoo! You've got blackJack! ";
-    hasBlackJack = true
-}else{
-  message = "You're out of the game";
-    isAlive= false
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.querySelector("#cards-el");
+
+function renderGame(){
+  sumEl.textContent = `Sum : ${sum}`;
+  cardsEl.textContent = "Cards: " + firstCard +" + " + secondCard
+  if(sum <= 20){
+      message = "Do you want to draw a new card? ";
+  }else if(sum === 21){
+    message ="Wohoo! You've got blackJack! ";
+      hasBlackJack = true;
+  }else{
+    message = "You're out of the game";
+      isAlive= false;
+  }
+  messageEl.textContent = message
 }
-console.log(message)
+function startGame(){
+  renderGame()
+}
+
+
+function newCard(){
+  let cardOne = 7;
+  sum +=cardOne;
+  renderGame()
+}
