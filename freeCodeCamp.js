7452aba7adca0076a33941d344b85c3007ebb253
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-04-20 12:11:06 +0200

    learning

---
+++ freeCodeCamp.js
@@ -0,0 +1,8 @@
+// get the last letter in string
+
+const allString = 'Your test output will go here'
+const lastLetterOfAllString = allString[allString.length - 1]
+// console.log(lastLetterOfAllString)// output will be e
+
+// get the value of the third-to-last letter
+// console.log(allString[allString.length - 3]) output e
