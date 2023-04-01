6c85b3a380053167d58402cb3ccdce00c4ade20e
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-04-01 18:13:48 +0200

    feat: includes method

--- Array/appendex.js
+++ Array/appendex.js
@@ -7,10 +7,20 @@ let newArray = [];
 
 // console.log("Map array : " + myArray.map((item) => item + 1));
 
-for (let j = 0; j < myArray.length; j++) {
-  if (myArray[j] % 2 === 0) {
-    newArray.push(myArray[j]);
+// for (let j = 0; j < myArray.length; j++) {
+//   if (myArray[j] % 2 === 0) {
+//     newArray.push(myArray[j]);
+//   }
+// }
+// console.log(myArray);
+// console.log("For Loop Filter : " + newArray);
+// console.log("Filter myArray : " + myArray.filter((item) => item % 2 === 0));
+
+function includeMethod(arr, value) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == value) return true;
   }
+  return false;
 }
-console.log("For Loop Filter : " + newArray);
-console.log("Filter myArray : " + myArray.filter((item) => item % 2 === 0));
+console.log(includeMethod(myArray, 2));
+console.log(myArray.includes(2));
