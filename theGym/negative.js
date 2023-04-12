8cd0aab9a317d71f85397cf7e8bdd830f5fb0f9b
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-04-12 11:12:20 +0200

    feat: shortcut solution

--- theGym/negative.js
+++ theGym/negative.js
@@ -1,5 +1,6 @@
 function makeNegative(num) {
   // Code?
-  return num > 0 ? -num : num;
+  //   return num > 0 ? -num : num; //this is my solution
+  return -Math.abs(num); // this is the short cut solution
 }
 console.log(makeNegative(-1));
