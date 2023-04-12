edfecc7db8dc9cf6e19a59b951b2f1633b319716
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-04-12 11:39:19 +0200

    feat: refactoring the challenge

--- theGym/betterThanAverage.js
+++ theGym/betterThanAverage.js
@@ -1,7 +1,3 @@
-function betterThanAverage(classPoints, yourPoints) {
-  // Your code here
-  let pointClas = classPoints.reduce((a, b) => a + b);
-  let av = pointClas / classPoints.length;
-  return yourPoints >= av;
-}
+const betterThanAverage = (classPoints, yourPoints) =>
+  yourPoints >= classPoints.reduce((a, b) => a + b) / classPoints.length;
 console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75));
