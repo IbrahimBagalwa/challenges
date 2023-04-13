e34cbfba4f5d547ac6744aba1cead5d02670b98a
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-04-13 15:19:25 +0200

    feat: check credit card

--- theGym/reg/creaditCard.js
+++ theGym/reg/creaditCard.js
@@ -1,6 +1,8 @@
-// Write a javascript program to check a credit card number.
+// Write a javascript program to check a credit card number is valid.
 // eg: 1234 5678 9876 5432
 function checkCreditCard(creditCard) {
-  return /^\d{4}\s\d{4}\s\d{4}\s\d{4}$/.test(creditCard);
+  return /^\d{4}\s\d{4}\s\d{4}\s\d{4}$/.test(creditCard)
+    ? "Valid Credit Card"
+    : "Invalid Credit Card";
 }
-console.log(checkCreditCard("1234 5678 9876 5432"));
+console.log(checkCreditCard("1234 56788 9876 5432"));
