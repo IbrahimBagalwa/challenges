5e621052844241f26d2406b99a97ef0ae12cc661
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-04-15 12:15:16 +0200

    feat: capturing regex

--- theGym/reg/regexcapturingGroup.js
+++ theGym/reg/regexcapturingGroup.js
@@ -7,3 +7,12 @@ function capturing(n) {
   return n.replace(reg, "$1-xxx-xxx-xxx");
 }
 console.log(capturing("(250)791-042-558")); //250-xxx-xxx-xxx
+
+// Bagalwa, Ibrahim
+// Swedi, Peter,
+// Bagalwa, Elijah
+const captureTheFirst = (w) => {
+  let regex = /(\w+),\s+(\w+)/;
+  return w.replace(regex, "$2,$1");
+};
+console.log(captureTheFirst("Bagalwa, Ibrahim")); //Ibrahim,Bagalwa
