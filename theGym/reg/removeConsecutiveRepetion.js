11ddd7f107ba880f351ca29fe944547f91dd9a99
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-04-16 22:44:34 +0200

    feat: remove consecutive repetion with filter and for loop

--- theGym/reg/removeConsecutiveRepetion.js
+++ theGym/reg/removeConsecutiveRepetion.js
@@ -7,8 +7,24 @@
 Words will be separated by a single space. There will be no leading or trailing spaces in the string. An empty string (0 words) is a valid input.
  */
 function removeConsecutiveRepetution(w) {
-  let reg = /\b(\w+)\s\b\1/;
-  return w.match(reg);
+  // first solution with for loop
+  /**
+   * for loop solution
+   * @param {
+   * let strArray = w.split(" ");
+   * let newArray = [];
+   * for (let i = 0; i < strArray.length; i++) {
+   * if (strArray[i] !== strArray[i + 1]) {
+   * newArray.push(strArray[i]);
+   * }
+   * }
+   * return newArray.join(" ");}
+   */
+  // with filter
+  return (strArr = w
+    .split(" ")
+    .filter((element, index, arr) => element !== arr[index + 1])
+    .join(" "));
 }
 console.log(
   removeConsecutiveRepetution(
