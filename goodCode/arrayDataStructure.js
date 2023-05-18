9568011c1045e0edd7f9e12db3bfc44092f27a09
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-05-18 17:28:09 +0200

    feat: array

--- goodCode/arrayDataStructure.js
+++ goodCode/arrayDataStructure.js
@@ -8,9 +8,9 @@ const strings = ["a", "b", "c", "d"];
 strings[0]; // O(1) // a => here there is no loop the computer knows where the value we want to access is stored.
 strings.push("e", "k"); // O(1) -> ["a", "b", "c", "d","e"] here as well we are not looping through the strings array we just add something at the end of the strings array;
 
-strings.pop(); // delete the last element. O(n)-> here is O(n) because we have to modify the index of our array
-
+strings.pop(); // delete the last element. O(1)
 strings.unshift("first"); // add the first index, O(n)-< here we have to loop over the array to change the indexes of items.
+strings.shift(); // delete the first element. O(n)-> here is O(n) because we have to modify the index of our array
 
 strings.splice(Math.floor(strings.length / 2), 0, "|"); // the big O here is O(n) why because the have again to loop over the array to change the indexes of items.
 
