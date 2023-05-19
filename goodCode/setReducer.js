0f94121621a2cc32476ccc2823a5dfb6f2c3019d
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-05-19 17:10:06 +0200

    feat: set reducer function

--- goodCode/setReducer.js
+++ goodCode/setReducer.js
@@ -11,7 +11,29 @@ function setReducer(input) {
   }
   return setReducer(newArr);
 }
-console.log(setReducer([0, 4, 6, 8, 8, 8, 5, 5, 7]));
+// console.log(setReducer([0, 4, 6, 8, 8, 8, 5, 5, 7]));
 
 // 'hello'-> 'l'-> 2
 // write an algorithm to get the maximum character in a string and how frequent that character appears
+
+const getMaxCharacter = (str) => {
+  const obj = {};
+  for (let char of str) {
+    !obj[char] ? (obj[char] = 1) : obj[char]++;
+  }
+
+  let maxNumber = 0;
+  let frequentChar = "";
+  for (const key in obj) {
+    if (obj[key] >= maxNumber) {
+      maxNumber = obj[key];
+      frequentChar = key;
+    }
+  }
+  console.log(`${frequentChar}:${maxNumber}`);
+};
+
+getMaxCharacter("frequent");
+
+// [1, 2, 5, 10], n = 2
+// [[1, 2], [5, 10]]
