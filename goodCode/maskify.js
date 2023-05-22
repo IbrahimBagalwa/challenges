499e2f082b0999e85a6446bf77621a4b7259361c
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-05-22 16:06:05 +0200

    feat:way that we can reverse a string

--- goodCode/maskify.js
+++ goodCode/maskify.js
@@ -5,4 +5,14 @@ function maskify(str) {
   }
   return ar.join("");
 }
-console.log(maskify("4556364607935616"));
+// console.log(maskify("4556364607935616"));
+
+function reverse(str) {
+  //   let arrString = str.split("");
+  //   let strings = [];
+  //   for (let i = arrString.length; i >= 0; i--) {
+  //     strings.push(arrString[i]);
+  //   }
+  return [...str].reverse().join("");
+}
+console.log(reverse("Hi my name is Ibrahim"));
