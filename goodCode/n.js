dd50f8d0b731206d4afed22f5c7f010bf4c101a1
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-05-25 23:11:13 +0200

    feat: length of character

--- goodCode/n.js
+++ goodCode/n.js
@@ -1,5 +1,8 @@
-const returnSingleDigit = (num) => {
-  return ((num - 1) % 9) + 1;
-};
+const returnSingleDigit = (num) => num % 9;
 
 console.log(returnSingleDigit(192));
+
+const str = (str, letter) => {
+  return str.split("").filter((it) => it === letter).length;
+};
+console.log(str("Hello", "l"));
