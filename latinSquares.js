f167b139276024f6970e9b04355e47c5e9ddb4d9
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-06-04 21:07:52 +0200

    feat: latin square

---
+++ latinSquares.js
@@ -0,0 +1,12 @@
+function makeLatinSquare(n) {
+  const square = Array.from({ length: n }, () => Array(n).fill(0));
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      square[j][i] = ((i + j) % n) + 1;
+    }
+  }
+  return square;
+}
+
+console.log(makeLatinSquare(2));
