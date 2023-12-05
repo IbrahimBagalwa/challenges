016803d04a115d0fbc5a00c79f0bdea26dc93bed
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-12-05 11:21:27 +0200

    feat: trebuchet challenges for advent code

--- december-challenges/part-two.js
+++ december-challenges/part-two.js
@@ -1,56 +1,46 @@
-/**
- * 
- * @param {*} str 
- * @returns 
- * 
---- Day 1: Trebuchet?! ---
-Something is wrong with global snow production, and you've been selected to take a look. The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
-
-You've been doing this long enough to know that to restore snow operations, you need to check all fifty stars by December 25th.
-
-Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
-
-You try to ask why they can't just use a weather machine ("not powerful enough") and where they're even sending you ("the sky") and why your map looks mostly blank ("you sure ask a lot of questions") and hang on did you just say the sky ("of course, where do you think snow comes from") when you realize that the Elves are already loading you into a trebuchet ("please hold still, we need to strap you in").
-
-As they're making the final adjustments, they discover that their calibration document (your puzzle input) has been amended by a very young Elf who was apparently just excited to show off her art skills. Consequently, the Elves are having trouble reading the values on the document.
-
-The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
-
-For example:
-    1abc2
-    pqr3stu8vwx
-    a1b2c3d4e5f
-    treb7uchet
-
-In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
-
-Consider your entire calibration document. What is the sum of all of the calibration values?
-To begin, get your puzzle input: https://adventofcode.com/2023/day/1/input
- */
-
-function firstAndLastNumbers(str) {
-  const array = [];
-  const firstNumber = parseInt(str.match(/\d/g));
-  const lastNumber = parseInt(str.split("").reverse().join("").match(/\d/g));
-  if (!isNaN(firstNumber) && !isNaN(lastNumber)) {
-    return `${firstNumber}${lastNumber}`;
+function partTwo(str) {
+  const digitsObj = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+  const firstDigitMatch = str.match(
+    /\d|(?:one|two|three|four|five|six|seven|eight|nine)/
+  );
+  const lastdigitsMatch = str.match(
+    /.*(\d|(?:one|two|three|four|five|six|seven|eight|nine))/
+  );
+  let v1 = "";
+  let v2 = "";
+  if (isNaN(+firstDigitMatch[0])) {
+    v1 += digitsObj[firstDigitMatch[0]];
   } else {
-    array.push(firstNumber);
-    array.push(lastNumber);
+    v1 += firstDigitMatch[0];
   }
-}
 
-function rebuchet(inputPuzzle) {
+  if (isNaN(+lastdigitsMatch[1])) {
+    v2 += digitsObj[lastdigitsMatch[1]];
+  } else {
+    v2 += lastdigitsMatch[1];
+  }
+
+  return `${v1}${v2}`;
+}
+function finalTest(inputPuzzle) {
   const lines = inputPuzzle.split("\n");
   const arrNumber = [];
   for (let i = 0; i < lines.length; i++) {
-    arrNumber.push(firstAndLastNumbers(lines[i]));
+    arrNumber.push(partTwo(lines[i]));
   }
-  const i = arrNumber.filter((item) => !isNaN(item));
-  return i.reduce((acc, cur) => Number(acc) + Number(cur), 0);
+  return arrNumber.reduce((acc, cur) => Number(acc) + Number(cur), 0);
 }
-
-const inputPuzzle = `gtlbhbjgkrb5sixfivefivetwosix
+const strings = `gtlbhbjgkrb5sixfivefivetwosix
 ninesixrgxccvrqscbskgzxh6cpvpxsqnb6
 dxxzrlzkksfsffp4
 sbzvmddhnjtwollnjv33d2lbcscstqt
@@ -1049,9 +1039,6 @@ oneqdlsb7sixllszjbceight
 xmqxqsixpgclxldnvlzvjm7nine4
 fourfive4tttldbmmkxvhqrmvmrkpxfzbd7
 44two1
-eightrtsjszc2
-Searching...
-`;
-const result = rebuchet(inputPuzzle);
-console.log(result);
-module.exports = rebuchet;
+eightrtsjszc2`;
+console.log(finalTest(strings));
+// 3eightwothree
