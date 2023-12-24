ac646c076fd01a56b3ff4c2a3961a267cb0cf8aa
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-12-24 21:44:06 +0200

    feat: working with hash map

---
+++ mental-models/module-1.js
@@ -0,0 +1,17 @@
+// let a = 10;
+// let b = a;
+// a = 0;
+// console.log(b);
+// console.log(a);
+
+// const copy = {
+//   title: "test title",
+//   name: "just javascript",
+//   author: "dan abramov",
+// };
+// copy.title =
+//   "just javascript the mental models  you really want to know javascript";
+// console.log(copy);
+
+const person = undefined;
+console.log(person.name);
